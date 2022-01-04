5d0bdd395999dec046eed79b0c1d24194c584381
Waffi Fatur Rahman <waffi.faturrahman@devstack.com.au>
2022-01-04 11:36:12 +0700

    Add Multithreading Knowledge

---
+++ 001-Multithreading/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+
+namespace _001_Multithreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main Thread Started");
+            //Creating Threads
+            Thread t1 = new Thread(Method1)
+            {
+                Name = "Thread1"
+            };
+            Thread t2 = new Thread(Method2)
+            {
+                Name = "Thread2"
+            };
+            Thread t3 = new Thread(Method3)
+            {
+                Name = "Thread3"
+            };
+            //Executing the methods
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            Console.WriteLine("Main Thread Ended");
+            Console.Read();
+        }
+        static void Method1()
+        {
+            Console.WriteLine("Method1 Started using " + Thread.CurrentThread.Name);
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Method1 :" + i);
+            }
+            Console.WriteLine("Method1 Ended using " + Thread.CurrentThread.Name);
+        }
+        static void Method2()
+        {
+            Console.WriteLine("Method2 Started using " + Thread.CurrentThread.Name);
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Method2 :" + i);
+            }
+            Console.WriteLine("Method2 Ended using " + Thread.CurrentThread.Name);
+        }
+        static void Method3()
+        {
+            Console.WriteLine("Method3 Started using " + Thread.CurrentThread.Name);
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Method3 :" + i);
+            }
+            Console.WriteLine("Method3 Ended using " + Thread.CurrentThread.Name);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Knowledge-Concurrency
+
+## What is Thread?
+Thread is a unit of a process that is responsible for executing the application code. 
+
+By default, every process has at least one thread which is responsible for executing the application code and that thread is called as Main Thread. So, every application by default is a single-threaded application.
+
+## What is Multithreading?
+
+A process can have multiple threads and each thread can perform a different task. In simple words, we can say that the three methods we define in our program can be executed by three different threads. 
+The advantage is that the execution takes place simultaneously (at the same time).
+
+See example code [here](001-Multithreading/Program.cs)
