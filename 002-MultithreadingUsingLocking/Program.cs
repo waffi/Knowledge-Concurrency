d76a180c3fc0e0dc3c01f7575b73533102f83749
Waffi Fatur Rahman <waffi.faturrahman@devstack.com.au>
2022-01-04 13:54:01 +0700

    Add MultithreadingUsingLocking Knowledge

---
+++ 002-MultithreadingUsingLocking/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace _002_MultithreadingUsingLocking
+{
+    class Program
+    {
+        static int Count = 0;
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(IncrementCount);
+            Thread t2 = new Thread(IncrementCount);
+            Thread t3 = new Thread(IncrementCount);
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            //Wait for all three threads to complete their execution
+            t1.Join();
+            t2.Join();
+            t3.Join();
+            Console.WriteLine(Count);
+            Console.Read();
+        }
+        private static object _lockObject = new object();
+        static void IncrementCount()
+        {
+            for (int i = 1; i <= 1000000; i++)
+            {
+                //Only protecting the shared Count variable
+                lock (_lockObject)
+                {
+                    Count++;
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -11,3 +11,11 @@ A process can have multiple threads and each thread can perform a different task
 The advantage is that the execution takes place simultaneously (at the same time).
 
 See example code [here](001-Multithreading/Program.cs)
+
+## Protecting Shared Resource in Multithreading Using Locking
+
+In a multithreading application, it is very important for us to handle multiple threads for executing critical section code. For example, if multiple threads want to access the shared resource then we need to protect the shared resource from concurrent access otherwise we will get some inconsistency output. 
+
+See example code [here](002-MultithreadingUsingLocking/Program.cs)
+
+Let us understand this with an example. In the following example, we are only protecting the shared Count variable from concurrent access.
