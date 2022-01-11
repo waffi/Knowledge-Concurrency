71f18591a48431a0ca790770ad951c11f4ca1edd
Waffi Fatur Rahman <waffi.faturrahman@devstack.com.au>
2022-01-11 13:15:05 +0700

    Add Deadlock Solution Knowledge

---
+++ 006_Deadlock_Solution/Account.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _006_Deadlock_Solution
+{
+    public class Account
+    {
+        public int ID { get; }
+        private double Balance;
+        public Account(int id, double balance)
+        {
+            ID = id;
+            Balance = balance;
+        }
+
+        public void WithdrawMoney(double amount)
+        {
+            Balance -= amount;
+        }
+        public void DepositMoney(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
---
+++ 006_Deadlock_Solution/AccountManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace _006_Deadlock_Solution
+{
+    public class AccountManager
+    {
+        private Account FromAccount;
+        private Account ToAccount;
+        private double TransferAmount;
+
+        public AccountManager(Account AccountFrom, Account AccountTo, double AmountTransfer)
+        {
+            FromAccount = AccountFrom;
+            ToAccount = AccountTo;
+            TransferAmount = AmountTransfer;
+        }
+
+        public void FundTransfer()
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name} trying to acquire lock on {FromAccount.ID}");
+            lock (FromAccount)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name} acquired lock on {FromAccount.ID}");
+                Console.WriteLine($"{Thread.CurrentThread.Name} Doing Some work");
+                Thread.Sleep(1000);
+                Console.WriteLine($"{Thread.CurrentThread.Name} trying to acquire lock on {ToAccount.ID}");
+
+
+                // before
+
+                //lock (ToAccount)
+                //{
+                //    FromAccount.WithdrawMoney(TransferAmount);
+                //    ToAccount.DepositMoney(TransferAmount);
+                //}
+
+                // after ( use try attempt to acquired lock )
+
+                if (Monitor.TryEnter(ToAccount, 3000))
+                {
+                    Console.WriteLine($"{Thread.CurrentThread.Name} acquired lock on {ToAccount.ID}");
+                    try
+                    {
+                        FromAccount.WithdrawMoney(TransferAmount);
+                        ToAccount.DepositMoney(TransferAmount);
+                    }
+                    finally
+                    {
+                        Monitor.Exit(ToAccount);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"{Thread.CurrentThread.Name} Unable to acquire lock on {ToAccount.ID}, So existing.");
+                }
+            }
+        }
+    }
+}
